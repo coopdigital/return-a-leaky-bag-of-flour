cdbf7e3ce546faec403bbb2d7aca55234e56fcb4
cfgreene <hello@studio-ran.com>
2020-11-23 14:41:28 +0000

    added dismiss functionality to modal

--- src/_js/main.js
+++ src/_js/main.js
@@ -4,3 +4,23 @@ $("#toggle").click(function(){
     $("#content").toggleClass("is--open");
     return false;
 });
+
+if ($(".feedback-form").length) {
+    var $box = $('.feedback-form');
+
+     // To center the box
+    var boxLeft = ($(window).width()) / 2 - ($box.width() / 2),
+    boxTop = ($(window).height()) / 2 - ($box.height() / 2);
+
+    $box.css({
+    left: boxLeft,
+    top: boxTop
+    });
+
+
+    $(".feedback-form__close").click(function(){
+        $('.feedback-form').hide();
+        return false;
+    });
+
+});
