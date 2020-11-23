b5648a13ea1d4ae1e7e189717dd7251f92917bb1
Chris Gibbons <chris.gibbons@coop.co.uk>
2020-11-23 15:34:20 +0000

    Update main.js
    Fixes lint error and makes modal resize correctly

--- src/_js/main.js
+++ src/_js/main.js
@@ -1,26 +1,39 @@
 var storeForms = new storeForms();
 
-$("#toggle").click(function(){
-    $("#content").toggleClass("is--open");
-    return false;
+$('#toggle').click(function () {
+  $('#content').toggleClass('is--open');
+  return false;
 });
 
-if ($(".feedback-form").length) {
+if ($('.feedback-form').length) {
+  var $box = $('.feedback-form');
+
+  // To center the box
+  var boxLeft = $(window).width() / 2 - $box.width() / 2,
+    boxTop = $(window).height() / 2 - $box.height() / 2;
+
+  $box.css({
+    left: boxLeft,
+    top: boxTop,
+  });
+
+  $('.feedback-form__close').click(function () {
+    $('.feedback-form').hide();
+    return false;
+  });
+}
+
+$(window).resize(function () {
+  if ($('.feedback-form').length) {
     var $box = $('.feedback-form');
 
-     // To center the box
-    var boxLeft = ($(window).width()) / 2 - ($box.width() / 2),
-    boxTop = ($(window).height()) / 2 - ($box.height() / 2);
+    // To center the box
+    var boxLeft = $(window).width() / 2 - $box.width() / 2,
+      boxTop = $(window).height() / 2 - $box.height() / 2;
 
     $box.css({
-    left: boxLeft,
-    top: boxTop
+      left: boxLeft,
+      top: boxTop,
     });
-
-
-    $(".feedback-form__close").click(function(){
-        $('.feedback-form').hide();
-        return false;
-    });
-
+  }
 });
